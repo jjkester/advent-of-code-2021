22f216c50d68ded9f20147f214f5970e701fbb6a
Jan-Jelle Kester <janjelle@jjkester.nl>
2021-12-03 00:12:35 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.0"
+}
+
+group = "nl.jjkester"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("reflect"))
+    testImplementation(kotlin("test"))
+    testImplementation("org.assertj:assertj-core:3.21.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
