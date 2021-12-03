6c7eb6e362ac7ace15f230e21d7ab71a646e65ab
Jan-Jelle Kester <janjelle@jjkester.nl>
2021-12-03 12:56:59 +0100

    Improvements to main

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.0"
+    application
 }
 
 group = "nl.jjkester"
@@ -23,4 +24,8 @@ tasks.test {
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "11"
+}
+
+application {
+    mainClass.set("nl.jjkester.adventofcode21.Main")
 }
--- src/main/kotlin/nl/jjkester/adventofcode21/Main.kt
+++ src/main/kotlin/nl/jjkester/adventofcode21/Main.kt
@@ -1,16 +1,29 @@
+@file:JvmName("Main")
 package nl.jjkester.adventofcode21
 
 import nl.jjkester.adventofcode21.boilerplate.format
 import nl.jjkester.adventofcode21.day01.Day01
 import nl.jjkester.adventofcode21.day02.Day02
+import nl.jjkester.adventofcode21.day03.Day03
 
 val days = setOf(
     Day01,
     Day02,
+    Day03,
 )
 
-fun main() {
-    days.map { it.day() }
+fun main(args: Array<String>) {
+    require(args.size <= 1)
+
+    days.map { it.day }
+        .run {
+            when (args.getOrNull(0)) {
+                "latest" -> this.maxByOrNull { it.day }?.let(::listOf) ?: emptyList()
+                null -> this
+                else -> emptyList()
+            }
+        }
+        .map { it() }
         .sorted()
         .map { it.format() }
         .forEach(::println)
